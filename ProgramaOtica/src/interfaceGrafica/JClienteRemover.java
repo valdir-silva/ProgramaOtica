5004a10e8eae321a4486d10c67da2dbd88cfd370
white <white@DESKTOP-L6Q8L7C>
2017-02-05 18:35:54 -0300

    conector .jar colocado no lugar certo

--- ProgramaOtica/src/interfaceGrafica/JClienteRemover.java
+++ ProgramaOtica/src/interfaceGrafica/JClienteRemover.java
@@ -6,12 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 
 public class JClienteRemover extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private static String server;
 	private static String user;
 	private static String key;
+	private JTextField textField;
 	//NAO TA USAAAANDO(server, user e key) LEMBRAR!
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -39,9 +41,19 @@ public class JClienteRemover extends JPanel {
 						
 		JPanel panelClienteRemover = new JPanel();
 		tabbedPane.addTab("Remover", null, panelClienteRemover, null);
+		panelClienteRemover.setLayout(null);
 		
 		JLabel lblNewLabel_1 = new JLabel("Remover");
+		lblNewLabel_1.setBounds(336, 5, 68, 14);
 		panelClienteRemover.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("id:");
+		lblNewLabel.setBounds(44, 95, 46, 14);
+		panelClienteRemover.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(66, 92, 86, 20);
+		panelClienteRemover.add(textField);
+		textField.setColumns(10);
 	}
-
 }
